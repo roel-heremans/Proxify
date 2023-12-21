40b181d342034f97d7a788b0e65dee7347c25db4
rheremans@sms1.local <rheremans@agtinternational.com>
2023-12-21 17:36:48 +0000

    remove matplotlib

--- Magenta/source/utils/dash_utils.py
+++ Magenta/source/utils/dash_utils.py
@@ -490,45 +490,6 @@ def import_xlsx_to_df(filename,
 
     return df[col_name_dict.values()], col_name_dict
 
-def make_trend_plot(df):
-
-    # Create subplots with shared x-axis
-    fig, axs = plt.subplots(4, 1, figsize=(10, 6), sharex=True)
-
-    # Plot original series
-    axs[0].plot(df['Temperature'], label='Original')
-    axs[0].plot(df['poly_fit'], label='Poly Fit', linestyle='--', color='red')
-    axs[0].legend(loc='upper left')
-
-    # Plot trend component
-    if 'trend' in df.columns:                    # then also seasonal and residue can be plotted
-        axs[1].plot(df['trend'], label='Trend')
-        axs[1].legend(loc='upper left')
-
-        # Plot seasonal component
-        axs[2].plot(df['seasonal'], label='Seasonal')
-        axs[2].legend(loc='upper left')
-
-        # Plot residue component
-        axs[3].plot(df['resid'], label='Residue')
-        axs[3].legend(loc='upper left')
-
-    # Plot Ground Truth
-    if 'GroundTruth' in df:
-        axs[3].plot(df['GroundTruth'], label='GT')
-        axs[3].legend(loc='upper left')
-
-    if 'trend' in df.columns:
-        threshold = 0.5  # Replace this with your desired threshold value
-        # Creating the 'pred' column based on the condition
-        df['pred'] = np.where(df['resid'] > threshold, threshold, np.nan)
-        axs[3].plot(df['pred'], label='Pred')
-        axs[3].legend(loc='upper left')
-
-    plt.tight_layout()  # Adjusts spacing between subplots for better visualization
-
-    return plt
-
 def make_trend_plotly(df):
     fig = make_subplots(rows=4, cols=1, shared_xaxes=True, vertical_spacing=0.05)
 
